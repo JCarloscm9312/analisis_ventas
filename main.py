c59125532b6fab3f4fb84d7e42e66ad1ef7d6abe
felipearaque98 <felipearaque98@gmail.com>
2024-10-01 14:14:32 -0500

    Se agrega codificacion para utf-8 y se eliminan los nulos de dataframe

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Carga de los datos
-datos = pd.read_csv("ventas.csv", sep = ";")
+datos = pd.read_csv("ventas.csv", sep = ";", encoding="utf-8")
 # Eliminar valores nulos y convertir la columna cantidad en valores enteros
-
+datos.dropna(inplace=True)
 # Filtrar solo productos
 
 # Realizar grafico de linea que nos muestre las ventas por año de los productos
