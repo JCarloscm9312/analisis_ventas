a2541535fdae9acf761d93ff02b1e087b4fd5216
felipearaque98 <felipearaque98@gmail.com>
2024-10-01 14:18:07 -0500

    Se cambia la codificacion del archivo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Carga de los datos
-datos = pd.read_csv("ventas.csv", sep = ";", encoding="utf-8")
+datos = pd.read_csv("ventas.csv", sep = ";", encoding="latin-1")
 # Eliminar valores nulos y convertir la columna cantidad en valores enteros
 datos.dropna(inplace=True)
 # Filtrar solo productos
